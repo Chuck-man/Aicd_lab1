c8f1613c937e15ccf11f82dce6eb5574fdf096d2
Chuck-man <sgold2398@gmail.com>
2022-12-14 20:57:33 +0400

    Доработка меню

--- Семестр 3, лаба 1/Source1.cpp
+++ Семестр 3, лаба 1/Source1.cpp
@@ -6,48 +6,76 @@
 
 int main() {
 
-	setlocale(LC_ALL, "ru");
-
 	bool exit = false;
 
 	while (!exit) {
-		Image picture(10, 10);
+		bool mean = 0;
+		int length = 0, width = 0;
+		try {
+			std::cout << "Create the image\n\n" << "Enter the length and width of the images: ";
+			std::cin >> length;
+			std::cin >> width;
+			std::cout << "Enter the value for the image: ";
+			std::cin >> mean;
+			Image picture(length, width, mean);
+			std::cout << picture << "\n\n";
+		}
+		catch (const char* err) {
+			std::cout << err << "\nДля продолжения нажмите любую кнопку.";
+			system("pause");
+		}
+		system("cls");
 		int sw;
-		std::cout<< "Выберете, из предложенного меню:\n1. Нарисовать прямоугольник\n2. Операции с картинками\n3. Операции с картинкой и значением\n4. Инверсия картинки\n5. Вывести картинку\n6. Выход\n\n";
+		std::cout << "Choose:\n1. Operations with images\n2. Operations with image and value\n3. Image inversion\n4. Calculate the occupancy rate\n5. Draw rectangle\n6. Reading/writing an element\n7. Exit\n";
 		std::cin >> sw;
 		system("cls");
 		switch (sw) {
-		case(1):
+		/*case(1):
 		{
-			int x1, y1, x2, y2;
-			std::cout << "Введите координаты левой верхней и правой нижней вершин прямоугольника не больше 10\n";
-			std::cin >> x1 >> y1 >> x2 >> y2;
-			picture.creating_rectangle(x1, y1, x2, y2);
-			std::cout << picture;
+			std::cout << "Enter the length and width of the images: ";
+			std::cin >> length >> width;
+			std::cout << "Enter the value for the image: ";
+			std::cin >> mean;
+			Image picture(length, width, mean);
+			double ratio = 0;
+			ratio = fullness(picture);
+			std::cout << "Occupancy rate  = " << ratio << "\n";
+			std::cout << picture << "\n";
 			system("cls");
 			break;
-		}
-		case(2):
+		}*/
+		case(1):
 		{
 			bool exit2 = false;
+			bool mean2, mean1;
+			std::cout << "Enter the length and width of the images: ";
+			std::cin >> length >> width;
+			std::cout << "Enter the value for the first image: ";
+			std::cin >> mean1;
+			std::cout << "Enter the value for the second image: ";
+			std::cin >> mean2;
+			Image picture1(length, width, mean1);
+			Image picture2(length, width, mean2);
+			system("cls");
+
 			while (!exit2) {
-				Image picture1(10, 10);
-				Image picture2(10, 10);
-				picture1.creating_rectangle(3, 2, 8, 8);
-				picture2.creating_rectangle(1, 2, 5, 7);
 				int sw2;
-				std::cout << "Выберете, из предложенного меню:\n1. Сложение картинок \n2. Перемножение картинок \n3. Выход\n\n";
+				std::cout << "Choose:\n1. Adding pictures \n2. Multiplication of images \n3. Exit\n\n";
 				std::cin >> sw2;
+				Image picture3 = picture1;
+				Image picture4 = picture2;
 				switch (sw2) {
 				case(1):
 				{
-					std::cout << picture1 + picture2;
+					std::cout << picture1 << "\n\n" << picture2 << "\n\n";
+					std::cout << picture3 + picture4;
 					system("cls");
 					break;
 				}
 				case(2):
 				{
-					std::cout << picture1 * picture2;
+					std::cout << picture1 << "\n\n" << picture2 << "\n\n";
+					std::cout << picture3 * picture4;
 					system("cls");
 					break;
 				}
@@ -61,42 +89,52 @@ int main() {
 			system("cls");
 			break;
 		}
-		case(3):
+		case(2):
 		{
 			bool value;
-			std::cout << "Значение переменной (0 или 1): ";
+			std::cout << "The meaning of the variable (0 or 1): ";
 			std::cin >> value;
 			bool exit3 = false;
+			if (value == 0) value = false;
+			else value = true;
+			bool mean3;
+			std::cout << "Enter the length and width of the image: ";
+			std::cin >> length >> width;
+			std::cout << "Enter the value for the image: ";
+			std::cin >> mean3;
+			system("cls");
+			Image picture3(length, width, mean3);
 			while (!exit3) {
-				if (value == 0) value = false;
-				else value = true;
-				Image picture3(10, 10);
-				picture3.creating_rectangle(3, 2, 7, 5);
 				int sw3;
-				std::cout << "Выберете, из предложенного меню:\n1. Сложение картинки и переменной \n2. Перемножение картинки и переменной \n3. Сложение переменной и картинки \n4. Перемножение переменной и картинки \n5. Выход\n\n";
+				std::cout << "Choose:\n1. Adding a picture and a variable \n2. Multiplication a picture and a variable \n3. Adding a variable and a picture \n4. Multiplication a variable and a picture \n5. Exit\n\n";
 				std::cin >> sw3;
+				Image picture4 = picture3;
 				switch (sw3) {
 				case(1):
 				{
-					std::cout << picture3 + value;
+					std::cout << "value = " << value <<"\n" << picture3 << "\n\n";
+					std::cout << picture4 + value;
 					system("cls");
 					break;
 				}
 				case(2):
 				{
-					std::cout << picture3 * value;
+					std::cout << "value = " << value << "\n" << picture3 << "\n\n";
+					std::cout << picture4 * value;
 					system("cls");
 					break;
 				}
 				case(3):
 				{
-					std::cout << value + picture3;
+					std::cout << "value = " << value << "\n" << picture3 << "\n\n";
+					std::cout << value + picture4;
 					system("cls");
 					break;
 				}
 				case(4):
 				{
-					std::cout << value * picture3;
+					std::cout << "value = " << value << "\n" << picture3 << "\n\n";
+					std::cout << value * picture4;
 					system("cls");
 					break;
 				}
@@ -109,19 +147,78 @@ int main() {
 			}
 			break;
 		}
+		case(3):
+		{
+			bool mean0;
+			std::cout << "Enter the length and width of the image: ";
+			std::cin >> length >> width;
+			std::cout << "Enter the value for the image: ";
+			std::cin >> mean0;
+			system("cls");
+			Image picture0(length, width, mean0);
+			std::cout <<"Original\n" << picture0 << "\nInversion\n" << !picture0;
+			system("cls");
+			break;
+		}
 		case(4):
 		{
-			std::cout << !picture;
+			bool mean0;
+			std::cout << "Enter the length and width of the image: ";
+			std::cin >> length >> width;
+			std::cout << "Enter the value for the image: ";
+			std::cin >> mean0;
+			system("cls");
+			Image picture0(length, width, mean0);
+			int x1 = 0, y1 = 0, x2 = 0, y2 = 0;
+			std::cout << "Enter the upper left and lower right coordinates of the rectangle: ";
+			std::cin >> x1 >> y1 >> x2 >> y2;
+			picture0.creating_rectangle(x1, y1, x2, y2);
+			double ratio = 0;
+			ratio = fullness(picture0);
+			std::cout << "Occupancy rate  = " << ratio << "\n" << picture0;
 			system("cls");
 			break;
 		}
 		case(5):
 		{
-			std::cout << picture;
+			bool mean0;
+			std::cout << "Enter the length and width of the image: ";
+			std::cin >> length >> width;
+			std::cout << "Enter the value for the image: ";
+			std::cin >> mean0;
+			system("cls");
+			Image picture0(length, width, mean0);
+			int x1, y1, x2, y2;
+			std::cout << "Enter the coordinates of the upper left and lower right vertices of the rectangle: \n";
+			std::cin >> x1 >> y1 >> x2 >> y2;
+			picture0.creating_rectangle(x1, y1, x2, y2);
+			std::cout << picture0;
 			system("cls");
 			break;
 		}
 		case(6):
+		{
+			bool value;
+			int x = 0, y = 0;
+			std::cout << "Enter the length and width of the images: ";
+			std::cin >> length >> width;
+			std::cout << "Enter the value for the image: ";
+			std::cin >> mean;
+			Image picture(length, width, mean);
+			std::cout << picture << "\n\n";
+			std::cout << "Enter the coordinates of the cell: ";
+			std::cin >> x >> y;
+			bool element = picture(x, y);
+			std::cout << element << "\n\n";
+			std::cout << "Enter the value to the cell: ";
+			std::cin >> value;
+			picture(x, y) = value;
+			std::cout << picture(x, y) << "\n\n";
+			std::cout << picture << "\n\n";
+			system("cls");
+			break;
+		}
+		case(7):
 		{
 			exit = true;
 			break;
